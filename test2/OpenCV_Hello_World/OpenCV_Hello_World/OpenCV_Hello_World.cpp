1d9cebc9d0095875965a2bc0800a24e7e084c610
TerehinAV <terehin_murom@mail.ru>
2015-07-06 11:46:53 +0400

    Test 2 Find Contours
    works

--- test2/OpenCV_Hello_World/OpenCV_Hello_World/OpenCV_Hello_World.cpp
+++ test2/OpenCV_Hello_World/OpenCV_Hello_World/OpenCV_Hello_World.cpp
@@ -14,35 +14,62 @@ int _tmain(int argc, _TCHAR* argv[])
 
     // Немножко размоем изображение. Просто так,
     //чтобы потрогать функции OpenCV
-	IplImage* smooth = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 3);
+	IplImage* smooth = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 3); // трехканальное изображение RGB
 	cvSmooth(src, smooth, CV_MEDIAN, 21, 21 );
     if(src==NULL){ /* Если вдруг такой картинки нет */
         printf("Кажется этот файл не существует");
         exit(0);
     }
-	IplImage* gray = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
+	IplImage* gray = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1); // одноканальное изображение полутоновое
 	
 	//cvCvtColor(src, gray, CV_BGR2GRAY); // преобразование в полутон исходного изображения
 	cvCvtColor(smooth, gray, CV_BGR2GRAY); // преобразование в полутон размытого изображения
 
-	IplImage* bin = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
+	IplImage* bin = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1); // одноканальное изображение двуцветное (бинарное)
 	cvThreshold(gray, bin, 50, 250, CV_THRESH_OTSU);
 
-    cvNamedWindow("src", CV_WINDOW_AUTOSIZE );
-	cvNamedWindow("smooth", CV_WINDOW_AUTOSIZE );
-	cvNamedWindow("gray", CV_WINDOW_AUTOSIZE );
-	cvNamedWindow("bin", CV_WINDOW_AUTOSIZE );
+	IplImage * canny = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1); // одноканальное изображение контурное (алгоритм Кенни)
+	//алгоритм Кенни
+	cvCanny(gray, canny, 10, 100, 3);
 
-    cvShowImage("src",src);
-	cvShowImage("smooth",smooth);
-    cvShowImage("gray",gray);
-	cvShowImage("bin",bin);
+    // находим контуры функцией cvFindContours
+
+	IplImage * contour = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1); // одноканальное изображение контурное
+
+	CvMemStorage* storage = cvCreateMemStorage(0);
+    CvSeq* contours=0;
+
+    int contoursCont = cvFindContours( bin, storage,&contours,sizeof(CvContour),CV_RETR_LIST,CV_CHAIN_APPROX_SIMPLE,cvPoint(0,0));
+
+    // нарисуем контуры
+	cvFloodFill(contour, cvPoint(0, 0), cvScalarAll(255)); //фон - белый
+		
+    for(CvSeq* seq0 = contours;seq0!=0;seq0 = seq0->h_next){
+                cvDrawContours(contour, seq0, CV_RGB(0,0,0), CV_RGB(0,0,0), 0, 1, 8); // рисуем контур
+    }
+
+    cvNamedWindow("src", CV_WINDOW_AUTOSIZE );
+	cvNamedWindow("canny", CV_WINDOW_AUTOSIZE );
+	cvNamedWindow("contour", CV_WINDOW_AUTOSIZE );
+	
+    cvShowImage("src", src);
+	cvShowImage("canny", canny);
+    cvShowImage("contour", contour);
     
 	//cvSaveImage("image.jpg",frame);     
     cvWaitKey();/* Ждем, пока вы что-нибудь не нажмете, 
                        иначе окошко просто "мелькнет" и вы
                        можете даже ничего не заметить */
-    cvDestroyAllWindows();
+            // освобождаем ресурсы
+        cvReleaseImage(&src);
+		cvReleaseImage(&smooth);
+        cvReleaseImage(&gray);
+        cvReleaseImage(&bin);
+
+        cvReleaseImage(&canny);        
+        cvReleaseImage(&contour);
+
+       	cvDestroyAllWindows();
 	return 0;
 }
 
